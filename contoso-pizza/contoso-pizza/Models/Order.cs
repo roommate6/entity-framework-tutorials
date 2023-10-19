c2c6a5423af86b74f59c2981acb826ec0396ff2f
roommate6 <floginstefan@gmail.com>
2023-10-19 22:10:58 +0300

    added the models of the application

---
+++ contoso-pizza/contoso-pizza/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace contoso_pizza.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public DateTime? OrderFulfilled { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; } = null!;
+        public ICollection<OrderDetail> OrderDetails { get; set; } = null!;
+    }
+}
---
+++ contoso-pizza/contoso-pizza/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contoso_pizza.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        [Column(TypeName = "decimal(6, 2")]
+        public decimal Price { get; set; }
+    }
+}
