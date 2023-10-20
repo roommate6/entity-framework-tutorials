2f894dcc72d1f27c5b1e0fe94dce4f421c12140a
roommate6 <floginstefan@gmail.com>
2023-10-20 17:18:47 +0300

    created a third version of the app that uses ASP.NET

---
+++ contoso-pizza/contoso-pizza-asp-net/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using contoso_pizza_asp_net.Data;
+using contoso_pizza_asp_net.Models;
+
+namespace contoso_pizza_asp_net.Pages.Products
+{
+    public class CreateModel : PageModel
+    {
+        private readonly contoso_pizza_asp_net.Data.ContosoPizzaContext _context;
+
+        public CreateModel(contoso_pizza_asp_net.Data.ContosoPizzaContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Products == null || Product == null)
+            {
+                return Page();
+            }
+
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ contoso-pizza/contoso-pizza-asp-net/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using contoso_pizza_asp_net.Data;
+using contoso_pizza_asp_net.Models;
+
+namespace contoso_pizza_asp_net.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly contoso_pizza_asp_net.Data.ContosoPizzaContext _context;
+
+        public IndexModel(contoso_pizza_asp_net.Data.ContosoPizzaContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Product = await _context.Products.ToListAsync();
+            }
+        }
+    }
+}
