ed10f7c3a7c7f21715fc09a34f8e5a3f20db9230
roommate6 <floginstefan@gmail.com>
2023-10-29 22:33:05 +0200

    Untrack files in .gitignore

--- super-hero-angular/backend/SuperHero.API/SuperHero.API/Controllers/SuperHeroController.cs
+++ super-hero-angular/backend/SuperHero.API/SuperHero.API/Controllers/SuperHeroController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHero.API.Data;
 
 namespace SuperHero.API.Controllers
 {
@@ -7,19 +9,17 @@ namespace SuperHero.API.Controllers
 	[ApiController]
 	public class SuperHeroController : ControllerBase
 	{
+		private readonly SuperHeroContext _context;
+
+		public SuperHeroController(SuperHeroContext context)
+		{
+			_context = context;
+		}
+
 		[HttpGet]
 		public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
 		{
-			return new List<SuperHero>
-			{
-				new SuperHero
-				{
-					Name = "Spider Man",
-					FirstName = "Peter",
-					LastName = "Parker",
-					Place = "New York"
-				}
-			};
+			return Ok(await _context.SuperHeroes.ToListAsync());
 		}
 	}
 }
---
+++ super-hero-angular/backend/SuperHero.API/SuperHero.API/Data/SuperHeroContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHero.API.Data
+{
+	public class SuperHeroContext : DbContext
+	{
+		public SuperHeroContext(DbContextOptions<SuperHeroContext> options) : base(options) { }
+
+		public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+	}
+}
